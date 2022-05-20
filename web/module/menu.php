f7be2a9bafee1692a3c8c52c30e770ac95754d24
leoleducq <leo49500@outlook.fr>
2022-05-20 17:25:15 +0200

    Le menu est stocké dans ce dossier

---
+++ web/module/menu.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <link rel="stylesheet" href="style/menu.css" />
+        <title>ADS B STATS</title>
+    </head>
+
+    <body>
+        <div id="menu">
+            <a href="../stats/">Accueil</a>
+            <a href="../stats/top.php">Classement</a>
+        </div>
+
+
+    </body>
+</html>
